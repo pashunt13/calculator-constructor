ba09c15472ee2f6bcb3d8ca0026d9921d33a5491
pashunt13 <pavelkozencev2002@gmail.com>
2023-03-19 18:56:01 +0300

    making layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div className="rectangle">
+          <div className="display">
+            <div className="display-nums">0</div>
+          </div>
+        </div>
+
+        <div className="rectangle">
+          <div className="math-group">
+            <button className="math">
+              <div className="math-value">/</div>
+            </button>
+            <button className="math">
+              <div className="math-value">x</div>
+            </button>
+            <button className="math">
+              <div className="math-value">-</div>
+            </button>
+            <button className="math">
+              <div className="math-value">+</div>
+            </button>
+          </div>
+        </div>
+
+        <div className="rectangle">
+          <div className="num-container">
+            <button className="math num">
+              <div className="math-value">7</div>
+            </button>
+            <button className="math num">
+              <div className="math-value">8</div>
+            </button>
+            <button className="math num">
+              <div className="math-value">9</div>
+            </button>
+            <button className="math num">
+              <div className="math-value">4</div>
+            </button>
+            <button className="math num">
+              <div className="math-value">5</div>
+            </button>
+            <button className="math num">
+              <div className="math-value">6</div>
+            </button>
+            <button className="math num">
+              <div className="math-value">1</div>
+            </button>
+            <button className="math num">
+              <div className="math-value">2</div>
+            </button>
+            <button className="math num">
+              <div className="math-value">3</div>
+            </button>
+            <button className="math num-0">
+              <div className="math-value">0</div>
+            </button>
+            <button className="math num">
+              <div className="math-value">,</div>
+            </button>
+          </div>
+        </div>
+
+        <div className="rectangle">
+          <button className="math to-count">
+            <div className="to-count-value">=</div>
+          </button>
+        </div>
+      </div>
+
+      <div className="switcher">
+        <div className="frame5">
+          <div className="frame4">
+            <div className="eye">
+              <div className="eye-outside"></div>
+              <div className="eye-inside"></div>
+            </div>
+            Runtime
+          </div>
+        </div>
+        <div className="frame6">
+          <div className="frame4">
+            <div className="g">{"<>"}</div>
+            Constructor
+          </div>
+        </div>
+      </div>
+
+      <div className="constructor">
+        <div className="gg">
+          <div className="text">Перетащите сюда</div>
+          <div className="text2">любой элемент из левой панели</div>
+        </div>
+      </div>
     </div>
   );
 }
